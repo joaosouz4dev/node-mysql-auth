ee45af8fc41fed137383ca709f38a815ce2ebcae
João Victor Souza <souza.joao1997@gmail.com>
2021-09-30 09:20:20 -0300

    feat: add rate limit

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-require('rootpath')();
-const express = require('express');
+require("rootpath")();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const errorHandler = require('_middleware/error-handler');
+const cors = require("cors");
+const errorHandler = require("_middleware/error-handler");
+const RateLimit = require("express-rate-limit");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
 // api routes
-app.use('/users', require('./users/users.controller'));
+app.use("/users", require("./users/users.controller"));
 
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => console.log('Server listening on port ' + port));
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+app.listen(port, () => console.log("Server listening on port " + port));
